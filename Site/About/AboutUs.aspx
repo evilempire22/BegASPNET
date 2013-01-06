6cc7dbaea12e1dc0be68d9aaad31776ba8fba160
evilempire22 <evilempire22@gmail.com>
2013-01-06 09:06:27 -0600

    Updated files

--- Site/About/AboutUs.aspx
+++ Site/About/AboutUs.aspx
@@ -2,6 +2,9 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="cpMainContent" Runat="Server">My name is Kyle.<br />
+  <asp:Button ID="Button1" runat="server" Text="Button" />
+  <Wrox:Banner ID="Banner2" runat="server" DisplayDirection="Horizontal" />
+&nbsp;
 </asp:Content>
 
---
+++ Site/Controls/Banner.ascx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Controls_Banner : System.Web.UI.UserControl
+{
+    public Direction DisplayDirection { get; set; }
+
+    public string NavigateUrl
+    {
+      get
+      {
+        object _navigateURL = ViewState["NavigateUrl"];
+
+        if (_navigateURL != null)
+        {
+          return (string)_navigateURL;
+        }
+        else
+        {
+          return "http://p2p.wrox.com"; //Return a default value
+        }
+      }
+      set
+      {
+        ViewState["NavigateUrl"] = value;
+      }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      HorizontalPanel.Visible = false;
+      VerticalPanel.Visible = false;
+
+      switch (DisplayDirection)
+      {
+        case Direction.Horizontal:
+          HorizontalPanel.Visible = true;
+          HorizontalLink.HRef = NavigateUrl;
+          break;
+
+        case Direction.Vertical:
+          VerticalPanel.Visible = true;
+          VerticalLink.HRef = NavigateUrl;
+          break;
+      }
+    }
+}
